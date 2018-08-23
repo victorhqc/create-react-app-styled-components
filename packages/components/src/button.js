ab53fbcf1c99dc8ccff91cf7215baeddcc50ab37
Victor Quiroz <victorhqc@gmail.com>
2018-08-23 08:55:37 +0200

    Init components

---
+++ packages/components/src/button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 30px;
+  background-color: lightcoral;
+  border-radius: 3px;
+  border: none;
+`;
+
+export default Button;
